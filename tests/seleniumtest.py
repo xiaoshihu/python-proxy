c13e7a2243e056ff500ea7b41b5cdbb7ffa3f255
xiaoshihu <38485245+xiaoshihu@users.noreply.github.com>
2019-05-27 00:01:11 +0800

    一个代理工具，内容非常的丰富，好好学习改造，用于selenium代理处理ajax请求，但是使用异步框架搭建的，理解起来还是有点困难的

--- tests/seleniumtest.py
+++ tests/seleniumtest.py
@@ -10,7 +10,7 @@ from selenium import webdriver
 chromeOptions = webdriver.ChromeOptions()
 
 # 设置代理
-chromeOptions.add_argument("--proxy-server=http://127.0.0.1:8080")
+chromeOptions.add_argument("--proxy-server=http://127.0.0.1:8889")
 # 一定要注意，=两边不能有空格，不能是这样--proxy-server = http://202.20.16.82:10152
 browser = webdriver.Chrome(options = chromeOptions)
 
